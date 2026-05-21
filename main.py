faa6466e7ed600b97148c2eede5f802ca4e44e49
gabrielecavallo <Gabri.cabal@gmail.com>
2026-05-21 14:22:51 +0200

    Change airport and route loading generating an internal UUID for each european airport and mapping the routes with the new id

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 from dataclasses import dataclass
+import numpy as np
+import math
 import csv
 
 # List with all the included countries in this simulation
@@ -53,9 +55,31 @@ included_countries = [
     "Vatican City",
 ]
 
+# Matrice di adiacenza
+# ---------------------------- nodo
+# |     a_sd
+# |
+# |
+# |
+# nodo
+# L'elemento a_sd indica il costo della connessione tra l'aereoporto s e l'aereoporto d (rispettivamente source e destination)
+# se è negativo (-1), non c'è connessione
+# se è > 0 allora c'è una connessione con il costo
+# se è = 0 allora è una connessione tra lo stesso aereoporto
+# Nel nostro caso il costo è la distanza tra i due aereoporti in linea d'aria
+# La riga indica l'aereoporto sorgente
+
+# Ogni volta che carico una rotta, aggiungo una tupla (i, j) in un set, se non esiste già
+# Così escludo tutti i duplicati delle rotte tra due aereoporti
+# Prendo solo le rotte che hanno stops = 0 (ossia solo rotte dirette senza scalo)
+
+# Poi faccio una funzione che converte una tupla (partenza, destinazione) nella sua distanza con la formula
+
+
 @dataclass
 class Airport:
-    id: int
+    dataset_id: int
+    new_id: int
     name: str
     city: str
     country: str
@@ -63,72 +87,134 @@ class Airport:
     longitude: float
     type: str
 
+@dataclass
+class Route:
+    company: str
+    airline_id: int
+    source_name: str
+    src_dataset_id: int             # Id of the source airport using the dataset one
+    destination_name: str
+    dest_dataset_id: int
+    sd: tuple[int, int]             # Tuple with source-destination using program-generated ids
+    stops: int
+
 def clear(input: str) -> str:
     return input.replace("\"", "")
 
-def load_file() -> list[Airport]:
+def load_european_airports() -> list[Airport]:
+    """Load all airports of european cities"""
     f = open("input/airports.dat")
     reader = csv.reader(f, delimiter=",", quotechar="\"")
     try:
-
         airports = []
+        id = 0
         for row in reader:
+            country = clear(row[3])
+            if country not in included_countries:
+                continue
+
             airports.append(Airport(
-                id = int(row[0]),
+                dataset_id= int(row[0]),
+                new_id = id,                # Each airport has his own new id
                 name=clear(row[1]),
                 city=clear(row[2]),
-                country=clear(row[3]),
+                country=country,
                 latitude=float(row[6]),
                 longitude=float(row[7]),
                 type=row[12]
             ))
+            id += 1
 
         return airports
     except ValueError as e:
         print(e)
 
-def filter_airports_by_box(lat_start: float, lat_end: float, long_start: float, long_end: float, airports: list[Airport]) -> list[Airport]:
+def load_routes(all_airports: list[Airport]) -> list[Route]:
     """
-    Filters the list of all the airports using a box of coordinates. All the airports that fit the box will be included, the other will be
-    removed
-    :param lat_start: Start of the box for latitude
-    :param lat_end:
-    :param long_start: Start of the box for longitude
-    :param long_end:
-    :param airports List of input airports
-    :raises ValueError if coordinate box is invalid
-    :return: The list of all the airports fitting the box
+    Load all routes from airport A to B ensuring that there are no stops between them and that they belong only to the list we want to work with
+    :param all_airports: List of all the airports we work with
+    :return:
     """
-    res = list()
-    # Input check
-    if lat_end < lat_start:
-        raise ValueError(f"Latitude box error {lat_start}-{lat_end}")
+    f = open("input/routes.dat")
+    ok_ids = list(map(lambda airport: airport.dataset_id, all_airports))
+    reader = csv.reader(f, delimiter=",", quotechar="\"")
 
-    if long_end < long_start:
-        raise ValueError(f"Longitude box error {long_start}-{long_end}")
+    try:
+        routes = []
+        for row in reader:
+            stops = int(row[7])
+            if stops == 0 and row[1] != "\\N" and row[3] != "\\N" and row[5] != "\\N":
+                # Only single-connections
+                s = int(row[3])
+                d = int(row[5])
 
-    for airport in airports:
-        if lat_start <= airport.latitude <= lat_end:
-            if long_start <= airport.longitude <= long_end:
-                res.append(airport)
+                if s in ok_ids and d in ok_ids:         # Check that the airport starts from an airport in the right area and ends in airport in area
+                    routes.append(Route(
+                        company=row[0],
+                        airline_id=int(row[1]),
+                        source_name=row[2],
+                        src_dataset_id=s,
+                        destination_name=row[4],
+                        dest_dataset_id=d,
+                        stops=stops,
+                        sd=(get_airport_with_dataset_id(s, all_airports).new_id, get_airport_with_dataset_id(d, all_airports).new_id)
+                    ))
 
-    return res
+        return routes
+    except ValueError as e:
+        print(e)
 
-def filter_airports_by_country(airports: list[Airport], ok_countries: list[str])-> list[Airport]:
-    return list(filter(lambda t: t.country in ok_countries, airports))
+def get_airport_with_dataset_id(id: int, a: list[Airport]) -> Airport | None:
+    return list(filter(lambda t: t.dataset_id == id, a))[0]
+
+def calc_distance(s_data: Airport, d_data: Airport) -> float:
+    s_lat_rad = np.radians(s_data.latitude)
+    s_long_rad = np.radians(s_data.longitude)
+    d_lat_rad = np.radians(d_data.latitude)
+    d_long_rad = np.radians(d_data.longitude)
+
+    R = 6371000     # Medium earth radius
+    d = 2 * R * math.asin(
+        math.sqrt(
+            math.pow((math.sin((s_lat_rad - d_lat_rad)/2)), 2) + (
+                math.cos(s_lat_rad) * math.cos(d_lat_rad) * math.pow(math.sin((s_long_rad - d_long_rad) / 2), 2)
+            )
+        ))
+
+    return d
+
+# def create_graph(airports: list[Airport], routes: list[Route]):
+#     """
+#     Create the graph and return the adjacency matrix
+#     :param airports:
+#     :param routes:
+#     :return:
+#     """
+#     tot_airports = len(airports)
+#     # Creo una matrice identità tot_airports x tot_airports
+#     adj_dict: dict[int, float] = dict()
+#
+#     for route in routes:
+#         s = route.src_dataset_id
+#         s_data = get_airport_with_dataset_id(s, airports)
+#         d = route.dest_dataset_id
+#         d_data = get_airport_with_dataset_id(d, airports)
+#
+#         if adj_dict[s][d] == -1:
+#             matrix[s][d] = calc_distance(s_data, d_data)
+#
+#     return matrix
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    all_airports = load_file()
-    # Bounding box for getting only the european countries, exluding Ucraine and Russia because they have lots of airports
-    min_lat = 34.0
-    max_lat = 72.0
-    min_lon = -25.0
-    max_lon = 22.0
+    european_airports = load_european_airports()
 
-    european_airports = filter_airports_by_country(all_airports, included_countries)
     print("European airports alghorithm by Gabriele & Umberto")
-    print(f"{len(european_airports)} loaded over a total of {len(all_airports)}")
+    print(f"{len(european_airports)}")
     print(european_airports)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    routes = load_routes(european_airports)
+    print(f"Loaded {len(routes)} routes")
+
+    #matrix = create_graph(european_airports, routes)
+    #print(matrix)
